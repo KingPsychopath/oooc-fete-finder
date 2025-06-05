1ab16836ea259ee169dd0ea38dec5925a189cbc2
Your Name <you@example.com>
2025-06-05 03:18:51 +0100

    Fete baseURL change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	basePath: "/fete",
+	assetPrefix: "/fete",
+	trailingSlash: true,
 };
 
 export default nextConfig;
